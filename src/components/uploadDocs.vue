87e531d9a8b82ec97cfc4e531d92ef7b6239062a
shehan@321 <shehan774690541@gmail.com>
2023-10-31 15:00:45 +0530

    upload document successfull !

---
+++ src/components/uploadDocs.vue
@@ -0,0 +1,125 @@
+<template>
+  <div id="app" class="container">
+    <h2 style="text-align: center">Upload Document</h2>
+    <section>
+      <b-field>
+        <b-upload v-model="dropFiles" multiple drag-drop>
+          <section class="section" v-if="!dropFiles">
+            <div class="has-text-centered">
+              <p>
+                <b-icon icon="upload" size="is-large"> </b-icon>
+              </p>
+              <p>Drop your files here or click to upload</p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
+      <div
+        @click="deleteDropFile(index)"
+        v-for="(file, index) in dropFiles"
+        :key="index"
+        class="text is-dark"
+        style="justify-content: center; align-items: center"
+      >
+        <ul>
+          <li><b>File Name : </b> {{ file.name }}</li>
+          <li><b>File Size : </b> {{ formatFileSize(file.size) }}</li>
+          <li><b>File Type : </b> {{ getFileType(file.type) }}</li>
+        </ul>
+
+        <div style="width: 100%; display: flex; justify-content: space-between">
+          <b-button @click="uploadForDb(file)">Upload</b-button>
+          <b-button @click="deleteDropFile()">Remove</b-button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      dropFiles: null,
+      uploafVisible: true,
+      base64Image:null,
+    };
+  },
+  methods: {
+    deleteDropFile(index) {
+      this.dropFiles.splice(index, 1);
+      this.dropFiles = null;
+    },
+    async uploadForDb(file) {
+      try {
+        const fileInput = file;
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          const base64String = reader.result;
+          const base64ImageWithoutPrefix = base64String.split(",")[1];
+          this.base64Image = `${file.type}~${base64ImageWithoutPrefix}`;
+
+          console.log(this.base64Image)
+
+
+          let requestBody = {
+            username: "myName",
+            document: this.base64Image,
+          };
+
+          console.log(requestBody);
+          axios
+            .post("http://localhost:5169/api/UserDocs/uploadDocs", requestBody)
+            .then((response) => {
+              if (response.status === 200) {
+                this.alertCustom("Successfull", "Successfull Uploading Your Document!", "OK")
+                console.log("Data added successfully.");
+              }
+              this.$router.push("/");
+            })
+            .catch((error) => {
+              this.alertCustom("Successfull", error , "OK")
+            });
+        };
+
+        reader.readAsDataURL(fileInput);
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    },
+    formatFileSize(size) {
+      if (size < 1024) {
+        return size + " B";
+      } else if (size < 1024 * 1024) {
+        return (size / 1024).toFixed(2) + " KB";
+      } else {
+        return (size / (1024 * 1024)).toFixed(2) + " MB";
+      }
+    },
+    getFileType(type) {
+      const imageTypes = ["image/jpeg", "image/png", "image/gif"];
+      const documentTypes = ["application/pdf", "text/plain"];
+
+      if (imageTypes.includes(type)) {
+        return "Image";
+      } else if (documentTypes.includes(type)) {
+        return "Document";
+      } else {
+        return "Other";
+      }
+    },
+    alertCustom(_title, _message, _confirmText) {
+      this.$buefy.dialog.alert({
+        title: _title,
+        message: _message,
+        confirmText: _confirmText,
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/views/docs.vue
+++ src/views/docs.vue
@@ -1,13 +1,46 @@
 <template>
-  <h1>Documents</h1>
+  <div class="content">
+    <div class="form">
+      <div>
+        <uploadDocs/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import uploadDocs from '@/components/uploadDocs.vue';
 
+export default {
+    components:{
+        uploadDocs
+    }
 }
 </script>
 
 <style>
-
+.content {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.form {
+  border: 1px solid black;
+  border-radius: 20px;
+  padding: 20px;
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+}
+.form p {
+  text-align: center;
+  transition: ease 0.05s;
+}
+.form p:hover {
+  text-align: center;
+  color: blue;
+}
 </style>
